f431d2087924ad1f139d5e5ddd93ff394a759a8b
UntoastedToast <45534729+UntoastedToast@users.noreply.github.com>
2023-03-16 23:31:42 +0100

    Some adjustments

--- scripts/mapFunction.js
+++ scripts/mapFunction.js
@@ -188,7 +188,7 @@ function printData(graffitiArray) {
         iconUrl: '../ressources/icons/sprayer_marker.png',
     
         iconSize:     [25, 60], // size of the icon
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        iconAnchor:   [15, 58], // point of the icon which will correspond to marker's location
         popupAnchor:  [-3, -70] // point from which the popup should open relative to the iconAnchor
     });
 
